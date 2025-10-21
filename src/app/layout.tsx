fb92aa90ff21ed50ed1ce05eccf106dbba1782f4
bangyazaka-hash <bangyazaka@gmail.com>
2025-10-21 16:15:03 +0700

    About

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,22 +28,23 @@ export default function RootLayout({
       <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="bg-[#F3C623] h-12 flex items-center justify-between px-4">
-          <h1 className="font-bold h-6 flex items-center px-2 text-2xl">Véloz</h1>
+        <header className="bg-[#f3c623] h-12 flex items-center justify-between px-4">
           <nav className="flex justify-end space-4 gap-4">
             <div className="font-bold text-lg, flex space-x-4">
+              <hr></hr>
+            <h1 className="font-bold h-6 flex items-center px-2 text-2xl">Véloz</h1>
+            <div className="flex absolute right-0 gap-4 ">
             <Link href="/">Home</Link>
-
             <div className="relative group"><Link href="/">Catalog</Link>
               <div className="absolute hidden group-hover:block bg-white z-10">
                 <div className="hover:bg-gray-200 text-center px-4 py-2"><Link href="Frame">Frame</Link></div>
                 <div className="hover:bg-gray-200 text-center px-4 py-2"><Link href="Wheel">Wheel</Link></div>
                 <div className="hover:bg-gray-200 text-center px-4 py-2"><Link href="Component">Component</Link></div>
               </div>
-
-            </div>
+              </div>
             <Link href="/About">About</Link>
             <Link href="/Contact">Contact Us</Link>
+            </div>
             </div>
             
           </nav>
