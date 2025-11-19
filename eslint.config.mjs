43e2600bf0be050313b4b0dea93da5934a938cb1
bangyazaka-hash <bangyazaka@gmail.com>
2025-11-19 14:44:21 +0700

    frame detail

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,8 @@ const eslintConfig = [
     ],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   },
 ];
