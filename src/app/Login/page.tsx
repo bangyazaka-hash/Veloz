c0950d7e4fb28dbe52967db878ed077ac649f160
bangyazaka-hash <bangyazaka@gmail.com>
2025-11-20 13:14:09 +0700

    upload gambar ke vercel-blob

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -31,7 +31,7 @@ export default function LoginPage() {
     <div className="flex font-[lexend] min-h-screen">
       <div className="w-1/2 bg-[#f3c623] flex flex-col items-center justify-center text-black">
         <Image 
-        src="/veloz.png" 
+        src="/Veloz.png" 
         alt="Login" 
         width={150} 
         height={150} 
