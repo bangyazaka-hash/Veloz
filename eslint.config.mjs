ca0d4a829d453b3e2ceb92eb94acb4b67b81ee59
bangyazaka-hash <bangyazaka@gmail.com>
2025-11-21 14:57:44 +0700

    upload gambar ke vercel-blob

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,11 +19,6 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
   },
 ];
 
